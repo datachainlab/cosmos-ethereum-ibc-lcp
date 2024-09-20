ebce9b0a0d12f28fb1b40d11c32f8614e65246da
Masanori Yoshida <masanori.yoshida@datachain.jp>
2024-09-20 16:32:43 +0900

    upgrade yui-relayer, ethereum-ibc-relay-{chain,prover} to the latest versions
    Signed-off-by: Masanori Yoshida <masanori.yoshida@datachain.jp>

--- relayer/main.go
+++ relayer/main.go
@@ -5,8 +5,8 @@ import (
 	"os"
 
 	"github.com/datachainlab/ethereum-ibc-relay-chain/pkg/relay/ethereum"
-	"github.com/datachainlab/ethereum-ibc-relay-chain/pkg/relay/ethereum/signers/hd"
 	ethereumlc "github.com/datachainlab/ethereum-ibc-relay-prover/relay"
+	"github.com/datachainlab/ibc-hd-signer/pkg/hd"
 	lcp "github.com/datachainlab/lcp-go/relay"
 	rawsigner "github.com/datachainlab/lcp-go/relay/signers/raw"
 	lcptm "github.com/datachainlab/lcp-go/relay/tendermint"
