b096ac2423e104124b3a6bddefab6f567dcfa25b
Jun Kimura <jun.kimura@datachain.jp>
2025-01-29 09:31:46 +0900

    add settings for zkDCAP e2e-test
    Signed-off-by: Jun Kimura <jun.kimura@datachain.jp>

--- tests/e2e/cases/tm2eth/scripts/gen_rly_config.sh
+++ tests/e2e/cases/tm2eth/scripts/gen_rly_config.sh
@@ -1,12 +1,46 @@
 #!/bin/sh
-set -eux
-LCP_BIN=${LCP_BIN:-lcp}
-TEMPLATE_DIR=./tests/e2e/cases/tm2eth/configs/templates
-CONFIG_DIR=./tests/e2e/cases/tm2eth/configs/demo
+set -ex
+
+IS_DEBUG_ENCLAVE=false
+if [ "$LCP_ENCLAVE_DEBUG" = "1" ]; then
+    IS_DEBUG_ENCLAVE=true
+fi
+
+TEMPLATE_DIR=${E2E_TEST_DIR}/configs/templates
+CONFIG_DIR=${E2E_TEST_DIR}/configs/demo
+
 ADDRESSES_DIR=./tests/e2e/chains/ethereum/contracts/addresses
+
+IBC_ADDRESS=$(cat $ADDRESSES_DIR/IBCHandler)
+LC_ADDRESS=$(cat $ADDRESSES_DIR/LCPClient)
+
 mkdir -p $CONFIG_DIR
-MRENCLAVE=$(${LCP_BIN} enclave metadata --enclave=./bin/enclave.signed.so | jq -r .mrenclave)
-IBC_ADDRESS=`cat $ADDRESSES_DIR/IBCHandler`
-LC_ADDRESS=`cat $ADDRESSES_DIR/LCPClient`
-jq -n -f ${TEMPLATE_DIR}/ibc-0.json.tpl --arg MRENCLAVE ${MRENCLAVE} --arg LC_ADDRESS $LC_ADDRESS > ${CONFIG_DIR}/ibc-0.json
-jq -n -f ${TEMPLATE_DIR}/ibc-1.json.tpl --arg MRENCLAVE ${MRENCLAVE} --arg IBC_ADDRESS ${IBC_ADDRESS} --arg LC_ADDRESS $LC_ADDRESS > ${CONFIG_DIR}/ibc-1.json
+if [ "$ZKDCAP" = true ]; then
+    jq -n \
+        --arg MRENCLAVE ${LCP_MRENCLAVE} \
+        --argjson IS_DEBUG_ENCLAVE ${IS_DEBUG_ENCLAVE} \
+        --arg LC_ADDRESS ${LC_ADDRESS} \
+        --arg RISC0_IMAGE_ID ${LCP_RISC0_IMAGE_ID} \
+        -f ${TEMPLATE_DIR}/ibc-0-zkdcap.json.tpl > ${CONFIG_DIR}/ibc-0.json
+
+    jq -n \
+        --arg MRENCLAVE ${LCP_MRENCLAVE} \
+        --argjson IS_DEBUG_ENCLAVE ${IS_DEBUG_ENCLAVE} \
+        --arg IBC_ADDRESS ${IBC_ADDRESS} \
+        --arg LC_ADDRESS ${LC_ADDRESS} \
+        --arg RISC0_IMAGE_ID ${LCP_RISC0_IMAGE_ID} \
+        -f ${TEMPLATE_DIR}/ibc-1-zkdcap.json.tpl > ${CONFIG_DIR}/ibc-1.json
+else
+    jq -n \
+        --arg MRENCLAVE ${LCP_MRENCLAVE} \
+        --argjson IS_DEBUG_ENCLAVE ${IS_DEBUG_ENCLAVE} \
+        --arg LC_ADDRESS ${LC_ADDRESS} \
+        -f ${TEMPLATE_DIR}/ibc-0.json.tpl > ${CONFIG_DIR}/ibc-0.json
+
+    jq -n \
+        --arg MRENCLAVE ${LCP_MRENCLAVE} \
+        --argjson IS_DEBUG_ENCLAVE ${IS_DEBUG_ENCLAVE} \
+        --arg IBC_ADDRESS ${IBC_ADDRESS} \
+        --arg LC_ADDRESS ${LC_ADDRESS} \
+        -f ${TEMPLATE_DIR}/ibc-1.json.tpl > ${CONFIG_DIR}/ibc-1.json
+fi
--- tests/e2e/scripts/init_lcp.sh
+++ tests/e2e/scripts/init_lcp.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 set -ex
 
-LCP_BIN=${LCP_BIN:-lcp}
-ENCLAVE_PATH=./bin/enclave.signed.so
-CERTS_DIR=./tests/certs
+LCP_BIN=${LCP_BIN:-./bin/lcp}
+LCP_ENCLAVE_PATH=${LCP_ENCLAVE_PATH:-./bin/enclave.signed.so}
 
+CERTS_DIR=./lcp/tests/certs
+
+echo "Generate LCP configuration"
+echo "Remove existing LCP configuration"
 rm -rf ~/.lcp
 
-enclave_key=$(${LCP_BIN} --log_level=off enclave generate-key --enclave=${ENCLAVE_PATH})
+echo "Create new LCP configuration"
+enclave_key=$(${LCP_BIN} --log_level=off enclave generate-key --enclave=${LCP_ENCLAVE_PATH})
 
 if [ -z "$SGX_MODE" ] || [ "$SGX_MODE" = "HW" ]; then
-    ${LCP_BIN} attestation ias --enclave=${ENCLAVE_PATH} --enclave_key=${enclave_key}
+    ${LCP_BIN} attestation ias --enclave=${LCP_ENCLAVE_PATH} --enclave_key=${enclave_key}
 else
-    ${LCP_BIN} attestation simulate --enclave=${ENCLAVE_PATH} --enclave_key=${enclave_key} --signing_cert_path=${CERTS_DIR}/signing.crt.der --signing_key=${CERTS_DIR}/signing.key
+    ${LCP_BIN} attestation simulate --enclave=${LCP_ENCLAVE_PATH} --enclave_key=${enclave_key} --signing_cert_path=${CERTS_DIR}/signing.crt.der --signing_key=${CERTS_DIR}/signing.key
 fi
