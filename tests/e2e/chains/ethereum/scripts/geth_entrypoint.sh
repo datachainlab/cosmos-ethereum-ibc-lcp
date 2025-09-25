393039a8c1c5f244590601c6c429dc9efececbed
Mitsuhiro Tanda <mitsuhiro.tanda@datachain.jp>
2025-09-25 17:40:21 +0900

    fix parameter name

--- tests/e2e/chains/ethereum/scripts/geth_entrypoint.sh
+++ tests/e2e/chains/ethereum/scripts/geth_entrypoint.sh
@@ -9,7 +9,7 @@ MINIMAL_SLOTS_PER_EPOCH=8
 FUSAKA_TIMESTAMP_DIFF=$((EPOCH_OSAKA * MINIMAL_SECONDS_PER_SLOT * MINIMAL_SLOTS_PER_EPOCH))
 FUSAKA_TIMESTAMP=$((GENESIS_TIMESTAMP + FUSAKA_TIMESTAMP_DIFF))
 
-sed -i.bak s/"\"fusakaTime\": 0/\"fusakaTime\": ${FUSAKA_TIMESTAMP}/" /execution/genesis.json
+sed -i.bak s/"\"osakaTime\": 0/\"osakaTime\": ${FUSAKA_TIMESTAMP}/" /execution/genesis.json
 
 # Init
 geth --datadir=/execution init --state.scheme hash --db.engine=leveldb /execution/genesis.json
