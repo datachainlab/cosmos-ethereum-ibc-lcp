124fb1c2b469ad5a6baac05902e290f159c7ebcd
yoshidan <naohiro.y@gmail.com>
2026-04-07 11:31:39 +0900

    Bump up geth and lodestar & suport fusaka

--- tests/e2e/chains/ethereum/scripts/geth_entrypoint.sh
+++ tests/e2e/chains/ethereum/scripts/geth_entrypoint.sh
@@ -2,14 +2,20 @@
 set -eux
 
 GENESIS_TIMESTAMP=${GENESIS_TIMESTAMP}
+EPOCH_LATEST_HF=${EPOCH_LATEST_HF}
 
 MINIMAL_SECONDS_PER_SLOT=6
 MINIMAL_SLOTS_PER_EPOCH=8
 
-PRAGUE_TIMESTAMP_DIFF=$((EPOCH_PECTRA * MINIMAL_SECONDS_PER_SLOT * MINIMAL_SLOTS_PER_EPOCH))
-PRAGUE_TIMESTAMP=$((GENESIS_TIMESTAMP + PRAGUE_TIMESTAMP_DIFF))
-
-sed -i.bak s/"\"pragueTime\": 0/\"pragueTime\": ${PRAGUE_TIMESTAMP}/" /execution/genesis.json
+# Set latest hard fork timestamp based on EPOCH_LATEST_HF
+# Currently: osakaTime for Fulu/Osaka (update when new HF is added)
+# When EPOCH_LATEST_HF=0, keep timestamp=0 (from genesis)
+# Otherwise, set to future timestamp
+if [ "$EPOCH_LATEST_HF" -gt 0 ]; then
+    LATEST_HF_TIMESTAMP_DIFF=$((EPOCH_LATEST_HF * MINIMAL_SECONDS_PER_SLOT * MINIMAL_SLOTS_PER_EPOCH))
+    LATEST_HF_TIMESTAMP=$((GENESIS_TIMESTAMP + LATEST_HF_TIMESTAMP_DIFF))
+    sed -i.bak s/"\"osakaTime\": 0/\"osakaTime\": ${LATEST_HF_TIMESTAMP}/" /execution/genesis.json
+fi
 
 # Init
 geth --datadir=/execution init --state.scheme hash --db.engine=leveldb /execution/genesis.json
